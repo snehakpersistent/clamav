74ac33554b38c79008ec82d21db81eca275b3b17
Sneha Kanekar1 <sneha.kanekar1@ibm.com>
2022-07-04 17:25:16 +0530

    add ppc support to workflow

---
+++ crosscompile.cmake
@@ -0,0 +1,11 @@
+# the name of the target operating system
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR ppc64le)
+
+# which compilers to use for C and C++
+set(CMAKE_C_COMPILER /opt/at15.0/bin/powerpc64le-linux-gnu-gcc)
+set(CMAKE_CXX_COMPILER /opt/at15.0/bin/powerpc64le-linux-gnu-g++)
+
+set(CMAKE_FIND_LIBRARY_SUFFIXES ".a")
+set(BUILD_SHARED_LIBS OFF)
+set(CMAKE_EXE_LINKER_FLAGS "-static")
